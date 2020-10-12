c503318e0988be6f723c961b6c6d428a20ec476c
Joseph <josephvarghese.rms@gmail.com>
2020-10-12 22:31:31 +0530

    Method to generate short urls working

--- app/controllers/api/v1/short_url_controller.rb
+++ app/controllers/api/v1/short_url_controller.rb
@@ -1,19 +1,25 @@
 class Api::V1::ShortUrlController < ApplicationController
 
+    before_action :generate_short_url, only: [:create]
+
     def create
-
-        short_url = ShortUrl.find_by(short_url_params)
-
-        if short_url.nil?
-            render json: {notice: "Not found"}
-        else
-            render json: short_url
-        end
-
+        @url_entry = ShortUrl.new(short_url_params.merge(url_hash: @short_url))
+        
+        unless @url_entry.save
+            respond_with_model_error @short_url
+          end
+          render json: { notice: "Data Inserted", data: @url_entry }, status: :ok
     end
 
 
     private
+
+        def generate_short_url
+            loop do
+                @short_url = SecureRandom.alphanumeric(6)
+                break @short_url unless ShortUrl.where(url_hash: @short_url).first
+            end
+        end
 
         def short_url_params
             params.permit(:user_id, :url)
