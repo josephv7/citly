1736d6d2fa91e8296e2f8f24852fd1c2e96b2eae
Joseph <josephvarghese.rms@gmail.com>
2020-10-21 00:38:20 +0530

    Displaying messge if there shortened urls are not present for a user

--- app/javascript/components/UrlList.js
+++ app/javascript/components/UrlList.js
@@ -8,38 +8,51 @@ const UrlList = ({ userUrlList }) => {
       pathname: `/logs/${id}`,
     });
   };
-  return (
-    <Card>
-      {/* <p>{JSON.stringify(userUrlList)}</p> */}
-      {userUrlList.map((item, index) => {
-        return (
-          <div
-            className="d-flex flex-row justify-content-around align-items-center w-100 flex-wrap"
-            key={index}
-          >
-            <button type="button" className="btn btn-light w-25 my-1" disabled>
-              {item.url}
-            </button>
-            <button type="button" className="btn btn-light w-50">
-              <a target="_blank" href={`http://${item.redirectionUrl}`}>
-                {item.redirectionUrl}
-              </a>
-            </button>
-            <button type="button" className=" btn btn-light w-10" disabled>
-              {item.count}
-            </button>
-            <button
-              type="button"
-              className=" btn btn-light w-10"
-              onClick={() => showLogs(item.id)}
+  if (userUrlList.length > 0) {
+    return (
+      <Card>
+        {userUrlList.map((item, index) => {
+          return (
+            <div
+              className="d-flex flex-row justify-content-around align-items-center w-100 flex-wrap"
+              key={index}
             >
-              View Logs
-            </button>
-          </div>
-        );
-      })}
-    </Card>
-  );
+              <button
+                type="button"
+                className="btn btn-light w-25 my-1"
+                disabled
+              >
+                {item.url}
+              </button>
+              <button type="button" className="btn btn-light w-50">
+                <a target="_blank" href={`http://${item.redirectionUrl}`}>
+                  {item.redirectionUrl}
+                </a>
+              </button>
+              <button type="button" className=" btn btn-light w-10" disabled>
+                {item.count}
+              </button>
+              <button
+                type="button"
+                className=" btn btn-light w-10"
+                onClick={() => showLogs(item.id)}
+              >
+                View Logs
+              </button>
+            </div>
+          );
+        })}
+      </Card>
+    );
+  }else {
+    return (
+      <div className="no-logs--container text-center">
+        <div className="d-flex flex-column justify-content-center align-items-center">
+          <h2>No Shortened URLs</h2>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default UrlList;
